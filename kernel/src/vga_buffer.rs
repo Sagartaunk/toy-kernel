01cb97c02ba69fd418e63a45b744d3205372adda
Sagar Taunk <sagartaunk@proton.me>
2026-06-25 19:17:19 +0530

    src: vga_buffer.rs: Implement Partial color code
    Implement `color` enum, `ColorCode` struct and helper structs and
    functions.
    Todo: Implement rest of the helper functions and wire them properly

---
+++ kernel/src/vga_buffer.rs
@@ -0,0 +1,56 @@
+//! This module contains display related functions
+//! and/or structs and enum's.
+
+/// Define all the colors here with there color code.
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PatrialEq, Eq)]
+#[repr(u8)]
+pub enum color {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+struct ColorCode(u8);
+
+impl ColorCode {
+    fn new(foreground: color, background: color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+#[repr(transparent)]
+struct Buffer {
+    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer,
+}
