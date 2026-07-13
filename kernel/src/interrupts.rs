294ed885c88696bfdcc8712ad8543bad859f5c4c
Sagar Taunk <sagartaunk@proton.me>
2026-07-13 09:57:27 +0530

    interrupt.rs: Add support for Timer Interrupts
    Implement support for Timer Interrupts.

--- kernel/src/interrupts.rs
+++ kernel/src/interrupts.rs
@@ -2,6 +2,7 @@
 
 #![feature(abi_x86_interrupt)]
 
+use crate::print;
 use crate::{gdt, println};
 use lazy_static::lazy_static;
 use pic8259::ChainedPics;
@@ -18,6 +19,7 @@ lazy_static! {
                 .set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
+        idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
         idt
     };
 }
@@ -46,3 +48,33 @@ pub const PIC2_OFFSET: u8 = 1;
 // Thus, leaving all the bits used for exception are seperate.
 pub static PICS: spin::Mutex<ChainedPics> =
     spin::Mutex::new(unsafe { ChainedPics::new(PIC1_OFFSET, PIC2_OFFSET) });
+
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum InterruptIndex {
+    Timer = PIC1_OFFSET,
+}
+
+impl InterruptIndex {
+    fn as_u8(self) -> u8 {
+        self as u8
+    }
+    fn as_usize(self) -> usize {
+        usize::from(self.as_u8())
+    }
+}
+
+/// Figures out wheather the primary or secondary PIC sent the interruypt and then uses
+/// the `command` and `data` ports to send an `EOI` signal to the respective controllers.
+///
+/// If the secondary PIC sent the interrupt, both PIC's need to be notified because the
+/// secondary PIC is connected to an input line of the primary PIC.
+extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    print!(".");
+    // SAFETY: The caller needs to be careful to use the correct interrupt vector number,
+    // otherwise an unset interrupt could be deleted or a system hang could take place.
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+    }
+}
